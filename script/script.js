98dfac8a7a5903979fa27a4bd5fa49c3e11e36a2
tuomasesp <101732066+tuomasesp@users.noreply.github.com>
2022-05-23 23:41:58 +0300

    Buttoneille eventejä

--- script/script.js
+++ script/script.js
@@ -1,2 +1,30 @@
 // Tuomas Polvinen
 
+let norja = document.getElementById("norja");
+let latvia = document.getElementById("latvia");
+let usa = document.getElementById("usa");
+let ruotsi = document.getElementById("ruotsi");
+let britti = document.getElementById("britti");
+let itävalta = document.getElementById("itävalta");
+let tshekki = document.getElementById("tshekki");
+
+norja.addEventListener("click", haeTulosNorja);
+latvia.addEventListener("click", haeTulosLatvia);
+usa.addEventListener("click", haeTulosUsa);
+ruotsi.addEventListener("click", haeTulosRuotsi);
+
+function haeTulosNorja() {
+    document.getElementById("norja_tulos").innerHTML="Voitto 5-0";
+}
+
+function haeTulosLatvia() {
+    document.getElementById("latvia_tulos").innerHTML="Voitto 2-1";
+}
+
+function haeTulosUsa() {
+    document.getElementById("usa_tulos").innerHTML="Voitto 4-1";
+}
+
+function haeTulosRuotsi() {
+    document.getElementById("ruotsi_tulos").innerHTML="Tappio voittolaukauksilla 2-3"
+}
