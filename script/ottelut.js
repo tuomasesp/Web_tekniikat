f5a112d90cc3f86bd66bf02262c23a9a946d86ed
tuomasesp <101732066+tuomasesp@users.noreply.github.com>
2022-05-27 00:01:37 +0300

    scriptit järjestykseen

--- script/ottelut.js
+++ script/ottelut.js
@@ -8,7 +8,11 @@ let britti = document.getElementById("britti");
 let itävalta = document.getElementById("itävalta");
 let tshekki = document.getElementById("tshekki");
 
+let väri = document.getElementById("tausta");
+
 // Buttonista painamalla kenttään buttonin alle ilmestyy kunkin ottelun lopputulos. 
+// Lisäksi buttonilla taustan väri vaihtuu vastaamaan valitun vastustajamaan väriteemaa
+// Tausta sekä tekstin väri muuttuvat
  
 norja.addEventListener("click", haeTulosNorja);
 latvia.addEventListener("click", haeTulosLatvia);
@@ -20,30 +24,45 @@ tshekki.addEventListener("click", haeTulosTshekki);
 
 function haeTulosNorja() {
     document.getElementById("norja_tulos").textContent="Voitto 5-0";
+    väri.style.background="red";
+    väri.style.color="blue";
 }
 
 function haeTulosLatvia() {
     document.getElementById("latvia_tulos").textContent="Voitto 2-1";
+    väri.style.background="darkred";
+    väri.style.color="white";
 }
 
 function haeTulosUsa() {
     document.getElementById("usa_tulos").textContent="Voitto 4-1";
+    väri.style.background="navy";
+    väri.style.color="white";
 }
 
 function haeTulosRuotsi() {
     document.getElementById("ruotsi_tulos").textContent="Tappio voittolaukauksilla 2-3";
+    väri.style.background="yellow";
+    väri.style.color="blue";
 }
 
 function haeTulosBritti() {
     document.getElementById("britti_tulos").textContent="Voitto 6-0";
+    väri.style.background="midnightblue"
+    väri.style.color="white";
+
 }
 
 function haeTulosItävalta() {
     document.getElementById("itävalta_tulos").textContent="Voitto 3-0";
+    väri.style.background="red";
+    väri.style.color="white";
 }
 
 function haeTulosTshekki() {
     document.getElementById("tshekki_tulos").textContent="Voitto 3-0";
+    väri.style.background="red";
+    väri.style.color="blue";
 } 
 
 // Lisätty tekstipätkä tyhjäksi jääneeseen laatikkoon.
--- script/suomi.js
+++ script/suomi.js
@@ -1,13 +1,13 @@
 // Tämän js:n tarkoituksena nostaa Granlund esille listalta
-// mouseoveria käyttäen. Muuttaa samalla tähti-laatikon muotoilun. 
+// mouseoveria käyttäen.  
 
-let nhl = document.querySelector("#granlund");
-let tähti = document.getElementById("tähti");
+let nhl = document.querySelector("#granlund")
+
 nhl.addEventListener("mouseover", näytäGranlund);
-tähti.addEventListener("mouseover", näytäGranlund);
-
+nhl.addEventListener("mouseleave", näytäGranlund);
 
 function näytäGranlund() {
-    nhl.classList="granlund";
-    tähti.classList="granlund";
+    nhl.classList.toggle("granlund");
+    
 }
+
